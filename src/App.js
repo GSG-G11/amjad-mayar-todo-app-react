b4dc02826dbab9b65aa1b515e9589ea8f1e2dcb2
mayar-kabaja <kabajamayar@gmail.com>
2022-03-30 17:46:10 +0300

    add task when uers submit form

--- src/App.js
+++ src/App.js
@@ -4,29 +4,78 @@ import Tasks from './components/Tasks';
 import Form from './components/Form';
 import Header from './components/Header';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isToggleOn: false,
+      task : {
+        id:Math.floor(Math.random()*1000),
+        name:'',
+        time:'',
+        desc:'',
+        done:false,
+      },
+      tasks : []
     };
-    this.handleToggle = this.handleToggle.bind(this);
+  
   }
 
-  handleToggle(e) {
+  handleToggle = (e)=> {
     e.preventDefault();
-
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
+  
+  addName =(e)=>{
+    const {time , desc , done} = this.state.task;
+    this.setState({
+      task : {
+        name: e.target.value,
+        time,
+        desc,
+        done,
+      }
+    })
+  }
+  addTime =(e)=>{
+    const {name , desc , done} = this.state.task;
+    this.setState({
+      task : {
+        name,
+        time: e.target.value,
+        desc,
+        done,
+      }
+    })
+  }
+  addDesc =(e)=>{
+    const {time , name , done} = this.state.task;
+    this.setState({
+      task : {
+        name,
+        time,
+        desc :e.target.value,
+        done,
+      }
+    })
+  }
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    this.state.tasks.push(this.state.task)
     this.setState((prevState) => ({
       isToggleOn: !prevState.isToggleOn,
     }));
   }
   render() {
-    const { isToggleOn } = this.state;
+    const { isToggleOn , tasks} = this.state;
     return (
       <div className='App'>
         <Header handleToggle={this.handleToggle} />
-        {isToggleOn ? <Form handleToggle={this.handleToggle} /> : ''}
-        <Tasks />
+        {isToggleOn ? <Form handleToggle={this.handleToggle} addName={this.addName} addTime={this.addTime} addDesc={this.addDesc} handleSubmit={this.handleSubmit}/> : ''}
+        <Tasks tasks={tasks}/>
       </div>
     );
   }
@@ -39,6 +88,6 @@ export default App;
 // done todo Tasks => TaskCard
 // done form (name,time,desc,submitBtn)
 
-// todo completed todos component
+// todo done todos component
 // todo uncompleted todos component
 // todo all todos component
