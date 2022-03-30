6176c60e11ce69a465fa107b3edc9bdb56c987f1
mayar-kabaja <kabajamayar@gmail.com>
2022-03-30 12:54:38 +0300

    create Header component & Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+import './styles/Form.css';
+
+function Form({handleToggle}){
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+    return(
+        <div className="modal">
+            <form onSubmit={handleSubmit}>
+                <i className="fa fa-times" onClick={handleToggle}></i>
+                <input type="text" className="nameInput" placeholder="Enter the task name" />
+                <input type="text" className="descInput" placeholder="Enter the time you need " />
+                <textarea  className="timeTextArea" placeholder="Enter the task description" />
+                <button className="submitBtn"  onClick={handleToggle}>submit</button>
+            </form>
+        </div>
+    )
+}
+export default Form;
