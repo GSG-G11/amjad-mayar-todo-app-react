38110a9bc212e157f2729ac29fa3da1e79eeedda
amjad <amjedyehia1998@gmail.com>
2022-03-30 10:00:44 +0300

    create init project's folder relates #1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return <div className='App'>app</div>;
+}
+
+export default App;
+
+// todo Header => title + button(create todo)
+// todo TaskCard (name,time,desc,edit,delete,doneIcon)
+// todo Tasks => TaskCard
+// todo form (name,time,desc,submitBtn)
